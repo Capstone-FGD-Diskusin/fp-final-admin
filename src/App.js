c6d17e9fb2da3dc91da4c37821dc55fac05a57a8
fatkurfaris <@fatkurrizal12@gmail.com>
2022-01-11 16:54:21 +0700

    dev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter ,Route, Routes } from "react-router-dom";
+import HomeLogin from './pages/HomeLogin/HomeLogin';
+import LoginAdmin from './pages/LoginAdmin/LoginAdmin';
+// import {NavbarLogin} from "./pages/"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<HomeLogin/>}/>
+          <Route path="/HomeLogin" element={<LoginAdmin/>}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/LoginAdmin/LoginAdmin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarLogin from '../../components/Navbarlogin/NavbarLogin'
+import { Container } from 'react-bootstrap'
+
+export default function LoginAdmin() {
+    return (
+        <div>
+            <NavbarLogin />
+            <Container></Container>
+        </div>
+    )
+}
