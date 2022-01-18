59bf91205711b2d3387590ff9d664d050186764a
fatkurfaris <@fatkurrizal12@gmail.com>
2022-01-19 06:02:13 +0700

    login

--- src/pages/HomeLogin/HomeLogin.jsx
+++ src/pages/HomeLogin/HomeLogin.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import LoginContent from '../../components/LoginContent/LoginContent'
+import NavbarHome from '../../components/NavbarHome/NavbarHome'
 
 export default function HomeLogin() {
     return (
         <div>
-
+            <NavbarHome />
+            <Container >
+                <Row>
+                    <Col sm={2}></Col>
+                    <Col sm={8}>
+                        <LoginContent />
+                    </Col>
+                    <Col sm={2}></Col>
+                </Row>
+            </Container>
         </div>
     )
 }
